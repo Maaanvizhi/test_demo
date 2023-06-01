2aa173ae2a9d8946ce1a60d3d1bee3fda303f9be
Maaanvizhi <maanthamizh@gmail.com>
2023-06-01 18:38:19 +0530

    updating the java class

--- MyFirstJavaProject/src/HelloWorld.java
+++ MyFirstJavaProject/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello to Java World from Eclipse IDE");
+		System.out.println("Second line to print here");
 
 	}
 
